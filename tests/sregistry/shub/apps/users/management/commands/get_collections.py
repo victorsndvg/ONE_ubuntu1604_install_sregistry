1d8638fe86dab1bc42d2d8cce723e3a44b0591be
victorsndvg <victorsv@gmail.com>
2017-11-30 15:20:22 +0100

    custom command tests. start playing with it

---
+++ tests/sregistry/shub/apps/users/management/commands/get_collections.py
@@ -0,0 +1,89 @@
+'''
+
+Copyright (c) 2017, Vanessa Sochat, All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
+from django.core.management.base import (
+    BaseCommand,
+    CommandError
+)
+
+from shub.apps.users.models import User
+from shub.apps.main.models import Collection
+from shub.apps.main.utils import get_collection_users
+from shub.logger import bot
+import re
+
+class Command(BaseCommand):
+    '''add superuser will add admin and manager privs singularity 
+    registry. The super user is an admin that can build, delete,
+    and manage images
+    '''
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('--username', dest='username', default=None, type=str)
+
+    help = "Generates a superuser for the registry."
+    def handle(self,*args, **options):
+        if options['username'] is None:
+            raise CommandError("Please provide a username with --username")
+
+        bot.debug("Username: %s" %options['username']) 
+
+        try:
+            user = User.objects.get(username=options['username'])
+        except User.DoesNotExist:
+            raise CommandError("This username does not exist.")
+
+        print(user.get_username())
+        print(user.get_full_name())
+        print(user.get_session_auth_hash())
+        print("authenticated: "+str(user.is_authenticated()))
+#        print(dir(user))
+#        user.email_user("SREGISTRY", "MASSAGE", from_email="registry@cesga.es")
+
+
+#        context = validate_credentials(user=user)
+#        collection = Collection.objects.filter(owner=user)
+        collections = Collection.objects.filter()
+        for collection in collections:
+            print(type(collection))
+            print("    uri: "+collection.get_uri())
+            print("    url: "+collection.get_absolute_url())
+            for container in collection.containers.all():
+                print("        uri: "+container.get_uri())
+                print("        url: "+container.get_absolute_url())
+                print("        path: "+container.get_image_path()) # print(container.get_download_url())
+
+#                for label in container.labels():
+#                    print(label)
+
+#            users = get_collection_users(collection)
+#            for user in users:
+#                print("    user: "+user)
+
